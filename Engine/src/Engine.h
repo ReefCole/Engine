13dea73cd2ff5113ee8b3d675c0340c39c46cf47
Reef <7649967@student.swin.edu.au>
2019-01-19 19:08:48 +1100

    no message

---
+++ Engine/src/Engine.h
@@ -0,0 +1,9 @@
+#pragma once
+
+//for use by engine applications
+
+#include "Engine/Application.h" 
+
+// Entry Point --------------------------
+#include Engine/Entrypoint.h
+// --------------------------
